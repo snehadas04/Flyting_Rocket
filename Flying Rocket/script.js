58f69f8757b45fffbad7300d7dbe452a7771184b
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-24 23:55:07 +0530

    Add files via upload

--- Flying Rocket/script.js
+++ Flying Rocket/script.js
@@ -1,4 +1,8 @@
 function stars(){
     let count = 50;
-    let scene = document.querySelector(.scene);
+    let scene = document.querySelector(".scene");
+    let i = 0;
+    while(i<count){
+        let star = document.createElement("i");
+    }
 }
